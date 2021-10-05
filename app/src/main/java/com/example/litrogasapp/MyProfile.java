09351c1820e277944f32a26e4cda37aeae786ba6
TharushiRachel <68675238+TharushiRachel@users.noreply.github.com>
2021-10-05 19:22:29 +0530

    Edit Profile interface done

--- app/src/main/java/com/example/litrogasapp/MyProfile.java
+++ app/src/main/java/com/example/litrogasapp/MyProfile.java
@@ -2,7 +2,9 @@ package com.example.litrogasapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class MyProfile extends AppCompatActivity {
 
@@ -10,5 +12,10 @@ public class MyProfile extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_my_profile);
+    }
+
+    public void sendMessage3(View view) {
+        Intent intent = new Intent(this, EditProfile.class);
+        startActivity(intent);
     }
 }
